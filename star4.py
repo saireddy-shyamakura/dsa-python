af1c934bcf7ca9a3059ef67053844ccdb3120006
SAI REDDY SHYAMAKURA <saireddy.shyamakura@gmail.com>
2024-04-10 17:09:13 +0530

    star patterns

---
+++ star4.py
@@ -0,0 +1,5 @@
+n=6
+for i in range(n) :
+    for j in range(i):
+        print(j+1,end="")
+    print()
---
+++ star5.py
@@ -0,0 +1,5 @@
+n=6
+for i in reversed(range(n)) :
+    for j in reversed(range(i)):
+        print("*",end="")
+    print()
